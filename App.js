21d3b73b12fb26bec198bb9da2c9c46c533d4b9d
zak <zakcroft@gmail.com>
2018-09-09 15:29:08 +0100

    add list

--- App.js
+++ App.js
@@ -1,13 +1,50 @@
-import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from "react";
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 
 export default class App extends Component {
+  state = {
+    placeName: "",
+    places: []
+  };
+
+  placeNameChangeHandler = name => {
+    this.setState({
+      placeName: name
+    });
+  };
+
+  onPressLearnMore = () => {
+    const trimmedName = this.state.placeName.trim();
+    if (trimmedName) {
+      this.setState(({ places }) => {
+        return {
+          places: [...places, trimmedName]
+        };
+      });
+    }
+  };
+
   render() {
+    const { places } = this.state;
+    const placesList = places.map((p, i) => <Text key={i}>{p}</Text>);
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <View style={styles.inputContainer}>
+          <TextInput
+            style={styles.placeInput}
+            placeholder="great place"
+            value={this.state.placeName}
+            onChangeText={this.placeNameChangeHandler}
+          />
+          <Button
+            style={styles.placeButton}
+            onPress={this.onPressLearnMore}
+            title="Add"
+            color="#841584"
+            accessibilityLabel="Learn more about this purple button"
+          />
+        </View>
+        <View>{placesList}</View>
       </View>
     );
   }
@@ -16,8 +53,23 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 20,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "flex-start"
   },
+  inputContainer: {
+    //flex: 1,
+    ///backgroundColor: "green",
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center"
+  },
+  placeInput: {
+    width: "70%"
+  },
+  placeButton: {
+    width: "30%"
+  }
 });
